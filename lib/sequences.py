452cf1d54f0ed1c9d767781c15c2b5430e374320
J Vance <justinleevance94@gmail.com>
2024-01-29 19:03:20 -0500

    this was hard

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,31 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    new_list = []
+    if length == 0:
+        print(new_list)
+        return
+    elif length == 1:
+        new_list.append(0)
+        print(new_list)
+        return
+    elif length == 2:
+        new_list = [0,1]
+        print(new_list)
+        return
+    else:
+        new_list = [0,1]
+        start1 = new_list[-2]
+        start2 = new_list[-1]
+        i = length - 2
+        while i > 0:
+            new_num = start1 + start2
+            new_list.append(new_num)
+            start1 = start2
+            start2 = new_num
+            i = i - 1
+        print(new_list)
+        return
+print_fibonacci(100)
+        
+            
